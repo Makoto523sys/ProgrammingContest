68b768c3f412d703c43e566d03ce2b51ea6d3a10
Makoto <ookunimasaharu@gmail.com>
2021-02-15 00:46:27 +0900

    twoopt実装中

--- nn.c
+++ nn.c
@@ -18,52 +18,6 @@ struct list {
   struct node* tail;
 };
 
-
-void initialize(struct list* p) {
-  p->head = (struct node*)malloc(sizeof(struct node));
-  p->tail = (struct node*)malloc(sizeof(struct node));
-  p->head->prev = 0;
-  p->head->next = p->tail;
-  p->tail->prev = p->head;
-  p->tail->next = 0;
-}
-
-void insertBefore(struct node* p, int v) {
-  struct node* newNode = (struct node*)malloc(sizeof(struct node));
-  newNode->value = v;
-  newNode->next = p;
-  p->prev->next = newNode;
-  newNode->prev = p->prev;
-  p->prev = newNode;
-}
-
-void insertAfter(struct node* p, int v) {
-  struct node* newNode = (struct node*)malloc(sizeof(struct node));
-  newNode->value = v;
-  newNode->prev = p;
-  p->next->prev = newNode;
-  newNode->next = p->next;
-  p->next = newNode;
-}
-
-void erase(struct node* p) {
-  p->prev->next = p->next;
-  p->next->prev = p->prev;
-  free(p);
-}
-
-void printNumbers(struct list* p) {
-  for (struct node* i = p->head->next; i->next != 0; i = i->next)
-    printf("%d ", i->value);
-  //printf("\n");
-}
-
-void printString(struct list* p) {
-  for (struct node* i = p->head->next; i->next != 0; i = i->next)
-    printf("%c", i->value);
-  //printf("\n");
-}
-
 struct point{
     int x;
     int y;
@@ -76,12 +30,22 @@ void read_tsp_data(char *filename, struct point p[MAX_N],int *np);
 int find_nearest(struct point p[MAX_N], int city, int n, bool visited[MAX_N]);
 bool is_all_true(bool visited[MAX_N], int n);
 void nn(struct point p[MAX_N],int n,struct list* tour);
+void printString(struct list* p);
+void printNumbers(struct list* p);
+void erase(struct node* p);
+void insertAfter(struct node* p, int v);
+void insertBefore(struct node* p, int v);
+void initialize(struct list* p);
+void solve(struct point p[MAX_N], int n, struct list* tour, bool optimize);
+void twoopt(struct point p[MAX_N], int n, struct list* tour);
+void reconstruction(struct node *v, struct node *w);
 
 int main(int argc, char *argv[]) {
   int  n;                   // 点の数
   struct point  p[MAX_N];   // 各点の座標を表す配列
-  struct list tour;   // 巡回路を表現する配列
+  struct list tour;   // 巡回路を表現するリスト
   initialize(&tour);
+  bool optimize = false;
 
   if(argc != 2) {
     fprintf(stderr,"Usage: %s <tsp_filename>\n",argv[0]);
@@ -91,13 +55,24 @@ int main(int argc, char *argv[]) {
   // 点の数と各点の座標を1番目のコマンドライン引数で指定されたファイルから読み込む
   read_tsp_data(argv[1],p, &n);
   // 最近近傍法による巡回路構築
-  nn(p,n,&tour);
+  solve(p, n, &tour, optimize);
   // 巡回路長を画面に出力
-  printf("%lf\n",tour_length(p,n,&tour));
+  //printf("%lf\n",tour_length(p,n,&tour));
   // 巡回路をテキストファイルとして出力
   write_tour_data("tour.dat",n,&tour);
 
   exit(EXIT_SUCCESS);
+}
+
+void printNumbers(struct list* p) {
+  for (struct node* i = p->head->next; i->next != 0; i = i->next)
+    printf("%d ", i->value);
+  //printf("\n");
+}
+void printString(struct list* p) {
+  for (struct node* i = p->head->next; i->next != 0; i = i->next)
+    printf("%c", i->value);
+  //printf("\n");
 }
 
 double tour_length(struct point p[MAX_N], int n, struct list* tour) {
@@ -128,30 +103,30 @@ void write_tour_data(char *filename, int n, struct list* tour){
     }
     fprintf(fp,"\n");
     fclose(fp);
-}
+    }
+
 void read_tsp_data(char *filename, struct point p[MAX_N],int *np) {
-  FILE *fp;
-  char buff[100];
-  int i;
+    FILE *fp;
+    char buff[100];
+    int i;
 
-  if ((fp=fopen(filename,"rt")) == NULL) {// 指定ファイルを読み込み用に開く
-    fprintf(stderr,"Error: File %s open failed.\n",filename);
-    exit(0);
-  }
+    if ((fp=fopen(filename,"rt")) == NULL) {// 指定ファイルを読み込み用に開く
+        fprintf(stderr,"Error: File %s open failed.\n",filename);
+        exit(0);
+    }
 
-  while((fgets(buff,sizeof(buff),fp)!=NULL)   // DIMENSION で始まる行に出会う
-	&&(strncmp("DIMENSION",buff,9)!=0)) ; // まで読み飛ばす.
-  sscanf(buff,"DIMENSION : %d",np);           // 点の数 *np を読み込む
+    while((fgets(buff,sizeof(buff),fp)!=NULL)   // DIMENSION で始まる行に出会う
+    &&(strncmp("DIMENSION",buff,9)!=0)) ; // まで読み飛ばす.
+    sscanf(buff,"DIMENSION : %d",np);           // 点の数 *np を読み込む
 
-  while((fgets(buff,sizeof(buff),fp)!=NULL)   // NODE_COORD_SECTIONで始まる
-	&&(strncmp("NODE_COORD_SECTION",buff,18)!=0)) ; // 行に出会うまで,
-                                                        // 読み飛ばす.
-  for(i=0;i<*np;i++) {                       // i=0 から i=(*np)-1まで
-    if(fgets(buff,sizeof(buff),fp)!=NULL)
-      sscanf(buff,"%*d %d %d",&(p[i].x),&(p[i].y)); // i番目の点の座標を読み込む
-  }
+    while((fgets(buff,sizeof(buff),fp)!=NULL)   // NODE_COORD_SECTIONで始まる
+    &&(strncmp("NODE_COORD_SECTION",buff,18)!=0)) ; // 行に出会うまで読み飛ばす.
+    for(i=0;i<*np;i++) {                       // i=0 から i=(*np)-1まで
+        if(fgets(buff,sizeof(buff),fp)!=NULL)
+            sscanf(buff,"%*d %d %d",&(p[i].x),&(p[i].y)); // i番目の点の座標を読み込む
+    }
 
-  fclose(fp);
+    fclose(fp);
 }
 
 int find_nearest(struct point p[MAX_N], int city, int n, bool visited[MAX_N]){
@@ -184,7 +159,6 @@ void nn(struct point p[MAX_N],int n,struct list* tour){
     //printf("Before insertBefore\n");
     insertBefore(tour->tail, start);
     //printf("After insertBefore\n");
-    printf("start while loop\n");
     while(!is_all_true(visited, n)){
         //printf("Before find_nearest\n");
         int next = find_nearest(p, tour->head->next->value, n, visited);
@@ -193,5 +167,66 @@ void nn(struct point p[MAX_N],int n,struct list* tour){
         //printf("Before insertAfter\n");
         insertBefore(tour->tail, next);
         //printf("After insertAfter\n");
+    }
+}
+
+void erase(struct node* p) {
+  p->prev->next = p->next;
+  p->next->prev = p->prev;
+  free(p);
+}
+
+void insertAfter(struct node* p, int v) {
+  struct node* newNode = (struct node*)malloc(sizeof(struct node));
+  newNode->value = v;
+  newNode->prev = p;
+  p->next->prev = newNode;
+  newNode->next = p->next;
+  p->next = newNode;
+}
+
+void insertBefore(struct node* p, int v) {
+  struct node* newNode = (struct node*)malloc(sizeof(struct node));
+  newNode->value = v;
+  newNode->next = p;
+  p->prev->next = newNode;
+  newNode->prev = p->prev;
+  p->prev = newNode;
+}
+
+void initialize(struct list* p) {
+  p->head = (struct node*)malloc(sizeof(struct node));
+  p->tail = (struct node*)malloc(sizeof(struct node));
+  p->head->prev = 0;
+  p->head->next = p->tail;
+  p->tail->prev = p->head;
+  p->tail->next = 0;
+}
+
+void solve(struct point p[MAX_N], int n, struct list *tour,bool optimize){
+    nn(p,n,tour);
+    if(optimize) twoopt(p, n, tour);
+}
+
+void twoopt(struct point p[MAX_N], int n, struct list *tour){
+    double old_dist;
+    double new_dist;
+    for(struct node* u = tour->head->next; u->next->next != NULL; u = u->next){
+        struct node* v = u->next;
+        for(struct node* w = tour->head->next; w->next->next != NULL; w = w->next){
+            struct node* x = w->next;
+            int a = u->value, b = v->value, c = w->value, d = x->value;
+            old_dist = dist(p[a], p[b]) + dist(p[c], p[d]);
+            new_dist = dist(p[a], p[c]) + dist(p[b], p[d]);
+            if(old_dist < new_dist) reconstruction(v, w);
+            else continue;
+        }
+    }
+}
+
+void reconstruction(struct node* v, struct node* w){
+    struct node *v_next = v->next;
+    struct node *w_prev = w->prev;
+    for(struct node *f = v_next; f != w; f = f->next){
     }
 }
